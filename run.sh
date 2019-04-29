b23785b7c6bcd53977fa85ff371f28f23a4e0f77
Adam Weis <aweis210@gmail.com>
2019-04-29 15:43:45 -0700

    for now copy the js file to public

--- run.sh
+++ run.sh
@@ -4,4 +4,6 @@ esy dune build &&
 esy dune build executable/IrminWeb.re &&
 esy dune build executable/IrminWeb.bc &&
 esy buildjs &&
-node ./_esy/default/store/b/irmin_web-74307f7a/default/executable/IrminWeb.js
+JSFILE=./_esy/default/store/b/irmin_web-74307f7a/default/executable/IrminWeb.js
+cp $JSFILE ~/public/. &&
+node $JSFILE
