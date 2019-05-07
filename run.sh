35d8bfc9789dbb574c3f915b29a1f9608ee2915c
Adam Weis <aweis210@gmail.com>
2019-05-07 16:10:06 -0700

    run.sh: add an optional param to put the code somewhere else

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
+WHERE_TO_PUT_CODE=$1
+
 esy buildjs &&
 JSFILE=`find . -name "IrminWebApp.bc.js"`;
 sed -i '1s|^|/**\n * @haste-ignore\n * @nolint\n */\n|' $JSFILE
 cp $JSFILE ~/public/. &&
+if [ -d "$WHERE_TO_PUT_CODE" ]; then
+  echo $WHERE_TO_PUT_CODE
+  cp "$JSFILE" "$WHERE_TO_PUT_CODE/Hecate.js"
+fi &&
 node $JSFILE
