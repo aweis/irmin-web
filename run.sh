6cb9ddd9622fde1c63f29afddc7f27197ac4b0c0
Adam Weis <aweis210@gmail.com>
2019-05-09 18:28:30 -0700

    preserve whitespace

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ WHERE_TO_PUT_CODE=$1
 
 esy buildjs &&
 JSFILE=`find . -name "IrminWebApp.bc.js"`;
-sed -i '1s|^|/**\n * @haste-ignore\n * @nolint\n */\n|' $JSFILE
+sed -i '1s|^|/**\n * @haste-ignore\n * @preserve-whitespace\n * @nolint\n */\n|' $JSFILE
 cp $JSFILE ~/public/. &&
 if [ -d "$WHERE_TO_PUT_CODE" ]; then
   echo $WHERE_TO_PUT_CODE
